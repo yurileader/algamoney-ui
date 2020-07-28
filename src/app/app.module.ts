7689315165f5697a1e556ea81c508bd2ddf8b2d6
Yuri Alves <yurileader@gmail.com>
2020-07-28 16:58:01 -0300

    criando componente Lancamento-pesquisa e Pessoa-pesquisa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LancamentoPesquisaComponent } from './lancamento-pesquisa/lancamento-pesquisa.component';
+
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
+import {TooltipModule} from 'primeng/tooltip';
+import { PessoaPesquisaComponent } from './pessoa-pesquisa/pessoa-pesquisa.component';
+
 
 @NgModule({
    declarations: [
       AppComponent,
-      NavbarComponent
+      NavbarComponent,
+      LancamentoPesquisaComponent,
+      PessoaPesquisaComponent
    ],
    imports: [
       BrowserModule,
-      AppRoutingModule
+      AppRoutingModule,
+      InputTextModule,
+      ButtonModule,
+      TableModule,
+      TooltipModule
    ],
    providers: [],
    bootstrap: [
